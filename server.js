0335aa65457cc6d0ec34ec9dec6ab90e804fb63b
philip fraser <pr.fraser3@gmail.com>
2017-11-15 10:34:38 +1000

    added mongoose and setup MLab mongoDB

--- server.js
+++ server.js
@@ -3,28 +3,43 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-	{name: "Phil", message: "hi"},
-	{name: "Bob", message: "hello"},
-]
+var dbUrl = 'mongodb://user:user@ds163745.mlab.com:63745/chat-prac-js'
+
+var Message = mongoose.model('Message', {
+	name: String,
+	message: String
+})
 
 app.get('/messages', (req, res) => {
-	res.send(messages)
+	Message.find({}, (err, messages) => {
+		res.send(messages)
+	})
 })
 
 app.post('/messages', (req, res) => {
-	messages.push(req.body);
-	io.emit('message', req.body)
-	res.sendStatus(200)
+	var message = new Message(req.body)
+	message.save((err) => {
+		if(err)
+			sendStatus(500)
+
+			io.emit('message', req.body)
+			res.sendStatus(200)
+	})
+
 })
 
 io.on('connection', (socket) => {
 	console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, {userMongoClient: true}, (err) => {
+	console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
