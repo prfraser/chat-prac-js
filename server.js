9b342e1a333973ce6710902bad10cea33bacad4a
philip fraser <pr.fraser3@gmail.com>
2017-11-15 11:09:56 +1000

    setup async await

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+mongoose.Promise = Promise
+
 var dbUrl = 'mongodb://user:user@ds163745.mlab.com:63745/chat-prac-js'
 
 var Message = mongoose.model('Message', {
@@ -22,16 +24,28 @@ app.get('/messages', (req, res) => {
 	})
 })
 
-app.post('/messages', (req, res) => {
-	var message = new Message(req.body)
-	message.save((err) => {
-		if(err)
-			sendStatus(500)
+app.post('/messages', async (req, res) => {
 
+	try {
+		var message = new Message(req.body)
+
+		var savedMessage = await message.save()
+
+		console.log('saved')
+
+		var censored = await Message.findOne({message: 'badword'})
+		if(censored)
+			await Message.remove({_id: censored.id})
+		else
 			io.emit('message', req.body)
-			res.sendStatus(200)
-	})
 
+		res.sendStatus(200)
+	} catch (error) {
+			res.sendStatus(500)
+			return console.error(error)
+	} finally {
+			console.log('finally ran')
+	}
 })
 
 io.on('connection', (socket) => {
